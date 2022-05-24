df3f01b5b85359579d97210ce7c3d991590e7a8a
xichenhe <xichenhe@ucsb.edu>
2022-05-24 12:17:52 -0700

    add import from firebase

--- cs184/lib/main.dart
+++ cs184/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:cs184/detail_page/detail_page.dart';
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'main_page/main_page.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
