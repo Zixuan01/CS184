36dcc8b20e98fcb3b96d5e2767939b668cb8fe3e
michaelwyl <yilin_wang@ucsb.edu>
2022-06-05 13:56:07 -0700

    added text field

--- cs184/lib/add_page/add_page.dart
+++ cs184/lib/add_page/add_page.dart
@@ -6,8 +6,6 @@ import 'package:flutter/cupertino.dart';
 
 enum Sky { expense, income }
 
-void main() => runApp(const AddPage());
-
 class AddPage extends StatefulWidget {
   const AddPage({Key? key}) : super(key: key);
   @override
@@ -15,34 +13,6 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
-  int _selected_index = 0;
-  static const option_style = TextStyle(
-    fontSize: 30,
-    fontWeight: FontWeight.bold,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return const CupertinoApp(
-      home: AddControl(),
-    );
-  }
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selected_index = index;
-    });
-  }
-}
-
-class AddControl extends StatefulWidget {
-  const AddControl({Key? key}) : super(key: key);
-
-  @override
-  State<AddControl> createState() => _AddControlState();
-}
-
-class _AddControlState extends State<AddControl> {
   Sky _selectedSegment = Sky.expense;
 
   Widget expanse = Container(
@@ -321,7 +291,7 @@ class _AddControlState extends State<AddControl> {
     ),
   );
 
-  Widget income = Container(
+  Widget income = SingleChildScrollView(
     padding: const EdgeInsets.symmetric(vertical: 30.0),
     child: Column(
       children: [
@@ -589,28 +559,6 @@ class _AddControlState extends State<AddControl> {
             ],
           ),
         ),
-        Padding(
-        padding: const EdgeInsets.symmetric(vertical: 20.0),
-        child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            TextField(
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(),
-                                labelText: 'Enter amount',
-                              ),
-                              keyboardType: TextInputType.number,
-                              onChanged: (value) {
-                                //setState(() {
-                                //  _amount = int.parse(value);
-                                //});
-                              },
-                            )
-
-                          ],
-                        ),
-                      ),
-        ),
       ],
     ),
   );
@@ -618,55 +566,76 @@ class _AddControlState extends State<AddControl> {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      backgroundColor: Colors.white,
-      navigationBar: CupertinoNavigationBar(
-        backgroundColor: Colors.black,
-        padding: EdgeInsetsDirectional.all(5.0),
-        // border: Border(bottom: BorderSide(width: 10.0)),
+        backgroundColor: Colors.white,
+        navigationBar: CupertinoNavigationBar(
+          backgroundColor: Colors.black,
+          padding: EdgeInsetsDirectional.all(5.0),
+          // border: Border(bottom: BorderSide(width: 10.0)),
 
-        leading: Material(
-            color: Colors.black,
-            child: IconButton(
-                onPressed: () {
-                  Navigator.of(context, rootNavigator: true).pop(context);
-                },
-                icon: const Icon(
-                  Icons.arrow_back_ios_new_rounded,
-                  color: Colors.white,
-                ))),
+          leading: Material(
+              color: Colors.black,
+              child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context, rootNavigator: true).pop(context);
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_ios_new_rounded,
+                    color: Colors.white,
+                  ))),
 
-        middle: CupertinoSlidingSegmentedControl<Sky>(
-          backgroundColor: CupertinoColors.systemGrey3,
-          thumbColor: CupertinoColors.systemGrey,
-          // This represents the currently selected segmented control.
-          groupValue: _selectedSegment,
-          // Callback that sets the selected segmented control.
-          onValueChanged: (Sky? value) {
-            if (value != null) {
-              setState(() {
-                _selectedSegment = value;
-              });
-            }
-          },
-          children: const <Sky, Widget>{
-            Sky.expense: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                'Expense',
-                style: TextStyle(color: CupertinoColors.white),
+          middle: CupertinoSlidingSegmentedControl<Sky>(
+            backgroundColor: CupertinoColors.systemGrey3,
+            thumbColor: CupertinoColors.systemGrey,
+            // This represents the currently selected segmented control.
+            groupValue: _selectedSegment,
+            // Callback that sets the selected segmented control.
+            onValueChanged: (Sky? value) {
+              if (value != null) {
+                setState(() {
+                  _selectedSegment = value;
+                });
+              }
+            },
+            children: const <Sky, Widget>{
+              Sky.expense: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'Expense',
+                  style: TextStyle(color: CupertinoColors.white),
+                ),
               ),
-            ),
-            Sky.income: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Text(
-                'Income',
-                style: TextStyle(color: CupertinoColors.white),
+              Sky.income: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Text(
+                  'Income',
+                  style: TextStyle(color: CupertinoColors.white),
+                ),
               ),
-            ),
-          },
+            },
+          ),
         ),
-      ),
-      child: _selectedSegment == Sky.expense ? expanse : income,
-    );
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _selectedSegment == Sky.expense ? expanse : income,
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0),
+                  child: Card(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Enter amount',
+                      ),
+                      keyboardType: TextInputType.number,
+                      onChanged: (value) {
+                        //setState(() {
+                        //  _amount = int.parse(value);
+                        //});
+                      },
+                    ),
+                  )),
+            ],
+          ),
+        ));
   }
 }
